a059774ef79cce5f2cc05f2bd2bfb1e56ee4f485
toddberreth <tmberret@ncsu.edu>
2020-05-26 16:11:11 -0400

    add avg-geolocation for map center

--- web/assets/js/sflmt.js
+++ web/assets/js/sflmt.js
@@ -2865,6 +2865,20 @@ function getPath(path) {
 }
 
 /**
+* Control Sidepanel
+**/
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+} 
+
+/**
 * Scale each dimension of an array -1:1
 **/
 
